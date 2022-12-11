5134d7a02e6770104982c501d787c6ada40c4e93
BaoNguyen <baonlh@galaxy.com.vn>
2022-12-11 18:24:49 +0700

    done

--- airflow/dags/db_handler/queries.py
+++ airflow/dags/db_handler/queries.py
@@ -1,34 +1,99 @@
 class PostgresStatements:
-    # Make sure list queries and list_columns correspond with list tables
-    # list_tables = ['users', 'orders', 'transactions', 'menus']
-    list_tables = ['users']
-    list_queries = [
-        'SELECT user_id, name, email, mobile FROM public.users',
-        'SELECT order_id, item_id, num, price FROM public.orders',
-        'SELECT user_id, order_id, paid_price, status FROM public.transactions',
-        'SELECT item_id, name, price FROM public.menus'
-    ]
-    list_columns = [
-        ['user_id', 'name', 'email', 'mobile'],
-        ['order_id', 'item_id', 'num', 'price'],
-        ['user_id', 'order_id', 'paid_ price', 'status'],
-        ['item_id', 'name', 'price']
-    ]
+    def __init__(self):
+        # self.list_tables = ['users', 'orders', 'transactions', 'menus']
+        self.list_tables = ['users']
+        self.list_columns = [
+            ['user_id', 'name', 'email', 'mobile'],
+            ['order_id', 'item_id', 'num', 'price'],
+            ['user_id', 'order_id', 'paid_price', 'status'],
+            ['item_id', 'name', 'price']
+        ]
+
+        self.queries_object = self._gen_queries()
+
+    def _gen_queries(self):
+        result = []
+        info = {}
+        for index, table in enumerate(self.list_tables):
+            info['table'] = self.list_tables[index]
+            info['columns'] = self.list_columns[index]
+            info['select_statement'] = \
+                'SELECT {0} FROM public.{1}'.format(', '.join(map(str, self.list_columns[index])),
+                                                    self.list_tables[index])
+            info['check_update_statement'] = \
+                'SELECT {0} FROM {1} WHERE updated_at > created_at'.format(
+                    ', '.join(map(str, self.list_columns[index])), self.list_tables[index])
+
+            result.append(info)
+            info = {}
+        return result
 
 
 class MysqlStatements:
-    # Make sure list queries and list_columns correspond with list tables
-    # list_tables = ['users', 'orders', 'transactions', 'menus']
-    list_tables = ['users']
-    list_queries = [
-        'SELECT user_id, name, email, mobile FROM report.users',
-        'SELECT order_id, item_id, num, price FROM report.orders',
-        'SELECT user_id, order_id, paid_price, status FROM report.transactions',
-        'SELECT item_id, name, price FROM report.menus'
-    ]
-    list_columns = [
-        ['user_id', 'name', 'email', 'mobile'],
-        ['order_id', 'item_id', 'num', 'price'],
-        ['user_id', 'order_id', 'paid_ price', 'status'],
-        ['item_id', 'name', 'price']
-    ]
+    def __init__(self):
+        self.list_tables = ['users', 'orders', 'transactions', 'menus']
+        self.list_columns = [
+            ['user_id', 'name', 'email', 'mobile'],
+            ['order_id', 'item_id', 'num', 'price'],
+            ['user_id', 'order_id', 'paid_price', 'status'],
+            ['item_id', 'name', 'price']
+        ]
+
+        self.queries_object = self._gen_queries()
+
+    def _gen_queries(self):
+        result = []
+        info = {}
+        for index, table in enumerate(self.list_tables):
+            info['table'] = self.list_tables[index]
+            info['columns'] = self.list_columns[index]
+            info['select_statement'] = \
+                'SELECT {0} FROM report.{1}'.format(', '.join(map(str, self.list_columns[index])),
+                                                    self.list_tables[index])
+            info['check_update_statement'] = \
+                'SELECT {0} FROM {1} WHERE updated_at > created_at'.format(
+                    ', '.join(map(str, self.list_columns[index])), self.list_tables[index])
+
+            result.append(info)
+            info = {}
+        return result
+
+
+class DWHStatements:
+    def __init__(self):
+        self.list_tables = ['users', 'orders', 'transactions', 'menus']
+        self.list_columns = [
+            ['user_id', 'name', 'email', 'mobile'],
+            ['order_id', 'item_id', 'num', 'price'],
+            ['user_id', 'order_id', 'paid_price', 'status'],
+            ['item_id', 'name', 'price']
+        ]
+
+        self.queries_object = self._gen_queries()
+
+    def _gen_queries(self):
+        result = []
+        info = {}
+        for index, table in enumerate(self.list_tables):
+            info['table'] = self.list_tables[index]
+            info['columns'] = self.list_columns[index]
+            info['select_statement'] = \
+                'SELECT {0} FROM raw_data.{1}'.format(', '.join(map(str, self.list_columns[index])),
+                                                      self.list_tables[index])
+            info['update_statment'] = ''
+            result.append(info)
+            info = {}
+        return result
+
+    # def gen_update_data_queries(self, data, table):
+    #     new_value = ''
+    #     for index, row in data.iterrows():
+    #         for col in data.columns[1:]:
+    #             temp = '{0} = {1} ,'.format(col, row[col])
+    #             new_value += temp
+    #
+    #         query_str = """
+    #                     UPDATE TABLE {0}
+    #                     SET {3} updated_at = now()
+    #                     WHERE {1} = {2}
+    #                 """.format(table, data.columns[0], row[data.columns[0]], new_value)
